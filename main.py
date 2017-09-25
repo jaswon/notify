7470ffa03a2fd6042b782a8d569ba0f38ee018e7
jaswon5791 <jasonwon628@gmail.com>
2017-09-25 12:13:20 -0400

    use discord instead of fb messenger

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-from fbchat import Client
-from fbchat.models import *
+import discord
+import asyncio
 from bs4 import BeautifulSoup as Bs
 from urllib.request import urlopen, Request
 
 import creds
-
-client = None
-
-def selfMessage(m):
-  if not client: client = Client(fb_email, fb_passw)
-  client.sendMessage(
-    m, 
-    thread_id=client.uid, 
-    thread_type=ThreadType.USER
-  )
+client = discord.Client()
+chan = discord.Object(id=creds.server)
 
 ### check if PAX East Registration is open
+headers={'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7',} 
+request = Request("http://east.paxsite.com/registration",None,headers)
 def isOpen():
-  headers={'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7',} 
-  request = Request("http://east.paxsite.com/registration",None,headers)
-  page = urlopen(request)
-  soup = Bs(page, 'html.parser')
+  soup = Bs(urlopen(request), 'html.parser')
   return soup.body.contents[5].h2.strong.string != 'Registration opens soon'
 
-print(isOpen())
+async def todo():
+  await client.wait_until_ready()
+  while not client.is_closed:
+    if isOpen():
+      await client.send_message(channel, "REGISTRATION OPEN GOGOGOGO")
+    await asyncio.sleep(30) # task runs every 60 seconds
+
+@client.event
+async def on_ready():
+  await client.send_message(chan, "notify_me started")
+
+client.loop.create_task(todo())
+client.run(creds.token)
