6383683148f7d7baaaeaaeb72819353c3fd290e1
Jason Won <jasonwon628@gmail.com>
2017-09-25 17:02:40 +0000

    update notify format

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from urllib.request import urlopen, Request
 
 import creds
 client = discord.Client()
-chan = discord.Object(id=creds.server)
+recipient = discord.Object(id=creds.recipient)
 
 ### check if PAX East Registration is open
 headers={'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7',} 
@@ -18,12 +18,12 @@ async def todo():
   await client.wait_until_ready()
   while not client.is_closed:
     if isOpen():
-      await client.send_message(channel, "REGISTRATION OPEN GOGOGOGO")
+      await client.send_message(recipient, "REGISTRATION OPEN GOGOGOGO")
     await asyncio.sleep(30) # task runs every 60 seconds
 
 @client.event
 async def on_ready():
-  await client.send_message(chan, "notify_me started")
+  await client.send_message(recipient, "notify_me started")
 
 client.loop.create_task(todo())
 client.run(creds.token)
