c802479ef536666fa48c7c9377002c7a58ac2e54
jaswon5791 <jasonwon628@gmail.com>
2017-09-25 10:56:43 -0400

    add check method (determines when to send push notification)

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from fbchat import Client
 from fbchat.models import *
+from bs4 import BeautifulSoup as Bs
+from urllib.request import urlopen, Request
 
-from creds import *
+import creds
 
-client = Client(fb_email, fb_passw)
+client = None
 
 def selfMessage(m):
+  if not client: client = Client(fb_email, fb_passw)
   client.sendMessage(
     m, 
     thread_id=client.uid, 
     thread_type=ThreadType.USER
   )
 
-selfMessage("hello me")
+### check if PAX East Registration is open
+def isOpen():
+  headers={'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7',} 
+  request = Request("http://east.paxsite.com/registration",None,headers)
+  page = urlopen(request)
+  soup = Bs(page, 'html.parser')
+  return soup.body.contents[5].h2.strong.string != 'Registration opens soon'
+
+print(isOpen())
