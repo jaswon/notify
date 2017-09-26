ab3cfcde162e68e67faa6f311465c517543275b2
Jason Won <jasonwon628@gmail.com>
2017-09-26 05:18:13 +0000

    urllib (sync) -> aiohttp (async)

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import discord
 import asyncio
+import aiohttp
+import datetime
 from bs4 import BeautifulSoup as Bs
-from urllib.request import urlopen, Request
 
 import creds
 client = discord.Client()
 recipient = discord.Object(id=creds.recipient)
 
 ### check if PAX East Registration is open
-headers={'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7',} 
-request = Request("http://east.paxsite.com/registration",None,headers)
-def isOpen():
-  soup = Bs(urlopen(request), 'html.parser')
-  return soup.body.contents[5].h2.strong.string != 'Registration opens soon'
+async def isOpen():
+  async with aiohttp.get("http://east.paxsite.com/registration") as r:
+    if r.status == 200:
+      res = await r.text()
+      soup = Bs(res, 'html.parser')
+      return soup.body.contents[5].h2.strong.string != 'Registration opens soon'
+  return false
 
 async def todo():
   await client.wait_until_ready()
   while not client.is_closed:
-    if isOpen():
+    if await isOpen():
       await client.send_message(recipient, "REGISTRATION OPEN GOGOGOGO")
+    else:
+      print('[{:%Y-%m-%d %H:%M:%S}] - no change'.format(datetime.datetime.now()))
     await asyncio.sleep(30) # task runs every 60 seconds
 
 @client.event
