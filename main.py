a1cce24bcf1a0413e7291dfcbd458921e348fcda
Jason Won <jasonwon628@gmail.com>
2017-09-29 14:47:07 +0000

    handle request errors, restructure

--- main.py
+++ main.py
@@ -9,24 +9,26 @@ client = discord.Client()
 recipient = discord.Object(id=creds.recipient)
 
 ### check if PAX East Registration is open
-async def isOpen():
-  async with aiohttp.get("http://east.paxsite.com/registration") as r:
-    if r.status == 200:
-      res = await r.text()
-      soup = Bs(res, 'html.parser')
-      try:
-        check = soup.body.contents[5].h2.strong.string 
-        return check is None or check != 'Registration opens soon'
-      except AttributeError:
-        return true
-  return false
+async def status():
+  try:
+    async with aiohttp.get("http://east.paxsite.com/registration") as r:
+      if r.status == 200:
+        soup = Bs(await r.text(), 'html.parser')
+        banner = soup.select(".message h2 strong")
+        if len(banner) and banner[0].text == "Registration opens soon":
+          return None
+        else:
+          return "Website changed"
+  except e:
+    return "Website error"
 
 # check every 30 seconds, notify if true
 async def todo():
   await client.wait_until_ready()
   while not client.is_closed:
-    if await isOpen():
-      await client.send_message(recipient, "REGISTRATION OPEN GOGOGOGO")
+    check = await status()
+    if check is not None:
+      await client.send_message(recipient,check)
     else:
       print('[{:%Y-%m-%d %H:%M:%S}] - no change'.format(datetime.datetime.now()))
     await asyncio.sleep(30)
